415ae7a632521c2f7653027d0be7077c8070978a
ngorostiaga29 <70031614+ngorostiaga29@users.noreply.github.com>
2020-11-20 04:01:55 -0300

    Add files via upload

---
+++ ARM$/app.js
@@ -0,0 +1,42 @@
+ScrollReveal().reveal('.cont_slider', {
+    delay: 200
+});
+ScrollReveal().reveal('.banner_main', {
+    delay: 200
+});
+ScrollReveal().reveal('.columnas', {
+    delay: 200
+});
+ScrollReveal().reveal('.banner_sec', {
+    delay: 200
+});
+
+
+ScrollReveal().reveal('.video1', {
+    delay: 200
+});
+ScrollReveal().reveal('.p1', {
+    delay: 200
+});
+ScrollReveal().reveal('.d1', {
+    delay: 200
+});
+ScrollReveal().reveal('.d2', {
+    delay: 200
+});
+ScrollReveal().reveal('.d10', {
+    delay: 200
+});
+
+ScrollReveal().reveal('.img_dia', {
+    delay: 200
+});
+ScrollReveal().reveal('.img_cir', {
+    delay: 200
+});
+ScrollReveal().reveal('.col4', {
+    delay: 200
+});
+ScrollReveal().reveal('.col3', {
+    delay: 200
+});
